e6709a14d99eb55ac9676b6e189ab258f71fdd13
Branch Vincent <branchevincent@gmail.com>
2020-12-22 18:16:37 -0500

    initial commit

---
+++ hooks/post_gen_project.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+# Update all poetry deps
+# https://github.com/python-poetry/poetry/issues/461
+poetry lock -q
+poetry show --tree | grep '^\w' | awk '{ print $1 }' | sed 's/$/@latest/' | xargs poetry add -q
+
+# Initialize git
+git init
+echo gh repo create '{{cookiecutter.project_slug}}' --private --confirm --description='{{cookiecutter.project_description}}'
+git add --all
+git commit -qm "chore: initial commit"
