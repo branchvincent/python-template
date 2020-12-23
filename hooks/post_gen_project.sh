bb8a337b781c7202e576ca46718913efeb0df84a
Branch Vincent <branchevincent@gmail.com>
2020-12-23 17:48:55 -0500

    add vscode

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -2,12 +2,14 @@
 
 set -euo pipefail
 
-# Update all poetry deps: https://github.com/python-poetry/poetry/issues/461
+# Update poetry deps: https://github.com/python-poetry/poetry/issues/461
 poetry lock -q
 poetry show --tree | grep '^\w' | awk '{ print $1 }' | sed 's/$/@latest/' | xargs poetry add -Dq
 
-# Initialize git repo
+# Initialize git
 git init
-echo gh repo create '{{cookiecutter.project_slug}}' --private --confirm --description='{{cookiecutter.project_description}}'
 git add --all
 git commit -qm "chore: initial commit"
+
+# Create GitHub repo
+echo gh repo create '{{cookiecutter.project_slug}}' --description='{{cookiecutter.project_description}}' --private --confirm
